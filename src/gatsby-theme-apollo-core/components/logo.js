9e9a4f11e6bbd6407b3e86384d96c3569717ca59
Mike Cowgill <mike@algohub.com>
2020-05-30 12:25:00 -0700

    Favicon and logos now working

--- src/gatsby-theme-apollo-core/components/logo.js
+++ src/gatsby-theme-apollo-core/components/logo.js
@@ -1,1 +1,21 @@
-export {ReactComponent as default} from '../../assets/algohub-logo.svg';
+import React from 'react';
+import styled from '@emotion/styled';
+import {ReactComponent as AlgoHubLogo} from '../../assets/algohub-logo.svg';
+
+const Wrapper = styled.div({
+    display: 'flex',
+    fontSize: 24
+});
+
+const StyledLogo = styled(AlgoHubLogo)({
+    height: '0.7857142857em',
+    marginTop: '0.07142857143em'
+});
+
+export default function Logo() {
+    return (
+        <Wrapper>
+        <StyledLogo />
+        </Wrapper>
+    );
+}
